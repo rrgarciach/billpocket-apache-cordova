a1be4ce44604289b525ae6873b9018f10b542828
Ruy Garcia <rrgarciach@gmail.com>
2015-04-10 01:14:11 -0500

    Plugin starts working!

--- src/android/BillPocketPlugin.java
+++ src/android/BillPocketPlugin.java
@@ -4,6 +4,34 @@ import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 import android.util.Log;
 import android.provider.Settings;
 import android.widget.Toast;
@@ -13,6 +41,9 @@ import org.json.JSONObject;
 
 public class BillPocketPlugin extends CordovaPlugin {
 	public static final String TAG = "BillPocket Plugin";
+    public static final int BP_INTENT_CODE = 666;
+    Activity activity;
+    CallbackContext callbackContext;
 
 	public BillPocketPlugin() {}
 
@@ -22,48 +53,47 @@ public class BillPocketPlugin extends CordovaPlugin {
 	}
 
 	@Override
-	public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		final int duration = Toast.LENGTH_SHORT;
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		Log.v(TAG,"BillPocketPlugin received: " + action);
-        if("sendPayment".equals(action)) {
-            cordova.getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-                    JSONArray data = args;
-                    this.sendPayment(data, callbackContext);
+//		final int duration = Toast.LENGTH_SHORT;
+        if ("sendPayment".equals(action)) {
+//            cordova.getActivity().runOnUiThread(new Runnable() {
+//                public void run() {
+//                    JSONArray data = args;
+                    this.sendPayment(args, callbackContext);
 //                    Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), action, duration);
 //                    toast.show();
 //                     callbackContext.success();
-                }
-            });
+//                }
+//            });
             return true;
         }
         Log.v(TAG,"MethodNotFound");
         return false; // Returning false results in a "MethodNotFound" error.
 	}
     
-    public void sendPayment(JSONArray args, CallbackContext callbackContext) {
+    private void sendPayment(JSONArray args, CallbackContext callbackContext) throws JSONException {
         Intent intent = new Intent("billpocket.payments.START");
 
-        String urlScheme = args.get("urlScheme");
+        String urlScheme = args.getString(0);
 
         if (urlScheme.length() > 0) {
             intent.putExtra("urlScheme", urlScheme);
         }
 
-        intent.putExtra("amount", args.get("amount"));
-        intent.putExtra("pin", args.get("pin"));
-        intent.putExtra("transaction", args.get("transaction"));
-        intent.putExtra("transactionid", args.get("transactionid"));
-        intent.putExtra("tip", args.get("tip"));
-        intent.putExtra("identifier", args.get("identifier"));
-        intent.putExtra("reference", args.get("reference"));
-        intent.putExtra("email", args.get("email"));
-        intent.putExtra("phone", args.get("phone"));
+        intent.putExtra("amount", args.getString(1));
+        intent.putExtra("pin", args.getString(2));
+        intent.putExtra("transaction", args.getString(3));
+        intent.putExtra("transactionid", args.getString(4));
+        intent.putExtra("tip", args.getString(5));
+        intent.putExtra("identifier", args.getString(6));
+        intent.putExtra("reference", args.getString(7));
+        intent.putExtra("email", args.getString(8));
+        intent.putExtra("phone", args.getString(9));
 
-        startActivityForResult(intent, BP_INTENT_CODE);
+        this.callbackContext = callbackContext;
+        cordova.getActivity().startActivityForResult(intent, BP_INTENT_CODE);
     }
-
-
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent){
@@ -75,13 +105,12 @@ public class BillPocketPlugin extends CordovaPlugin {
         String response = extras.getString("result");
 
         if(response.equals("aprobada")){
-            Intent intenAprovado = new Intent(getActivity(), MenuActivity.class);
-            startActivity(intenAprovado);
-            Toast.makeText(getActivity(),"Accion realizada con exito",Toast.LENGTH_SHORT).show();
+            Toast.makeText(cordova.getActivity(),"Accion realizada con exito",Toast.LENGTH_SHORT).show();
         } else if (response.equals("declinada")){
-            Toast.makeText(getActivity(),"transacion rechazada",Toast.LENGTH_SHORT).show();
+            Toast.makeText(cordova.getActivity(),"transacion rechazada",Toast.LENGTH_SHORT).show();
         }else{
-            Toast.makeText(getActivity(),"ERROR DE BILLPOCKET",Toast.LENGTH_SHORT).show();
+            Toast.makeText(cordova.getActivity(),"ERROR DE BILLPOCKET",Toast.LENGTH_SHORT).show();
         }
+        this.callbackContext.success(response);
     }
 }
--- www/BillPocketPlugin.js
+++ www/BillPocketPlugin.js
@@ -2,8 +2,8 @@ console.log("Setting BillPocketPlugin...");
 var exec = require('cordova/exec');
 
 module.exports = {
-	console.log("BillPocketPlugin.js: added to scope.");
 	sendPayment: function(arrParams) {
-	exec( function(result){ console.log("OK" + reply) }, function(result){ console.log("Error" + reply) }, "BillPocketPlugin", "sendPayment", arrParams);
+        console.log("BillPocketPlugin.js: added to scope.");
+        exec( function(result){ console.log("OK" + reply) }, function(result){ console.log("Error" + reply) }, "BillPocketPlugin", "sendPayment", arrParams);
 	}
 };
